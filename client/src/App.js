78bb18aa06435cb9688ba89cf207eb88506e8cd7
cephus <pnakitaren2@gmail.com>
2023-12-26 10:09:46 -0500

    chore:integrate react nginx,redux with django

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import  React from 'react'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Real Estate</h1>
+      <p>You are in the real estate....</p>
+    </div>
+  );
+}
+
+export default App;
